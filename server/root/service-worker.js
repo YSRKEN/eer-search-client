f82316f32e55114171dc61680dd7a0c544b786a1
YSRKEN <ysr.ken@gmail.com>
2020-09-03 22:08:39 +0900

    [update] クライアントを更新したことに伴い、デプロイ分も更新

--- server/root/service-worker.js
+++ server/root/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.164e6b689fed6922b7c1ae644feee672.js"
+  "/precache-manifest.416f199672dba973207796579dad36e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
