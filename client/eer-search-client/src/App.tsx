f27d33317819e0de41718c2db4c594c6cefd8be5
YSRKEN <ysr.ken@gmail.com>
2021-09-02 10:35:06 +0900

    [Add] 中古検索結果機能を強化

--- client/eer-search-client/src/App.tsx
+++ client/eer-search-client/src/App.tsx
@@ -16,6 +16,8 @@ interface NewItem {
 interface UsedItem {
   price: number;
   name: string;
+  rank: string;
+  shop_name: string;
   item_url: string;
   image_url: string;
 }
@@ -157,7 +159,7 @@ const ResultNewView: React.FC<{
 
   return (<>
     <div className="row mt-3 justify-content-center">
-      <div className="col-12 col-md-8">
+      <div className="col-12 col-md-10">
         <form>
           <div className="form-group d-flex my-0">
             <label className="text-nowrap mr-3 mt-2" htmlFor="filterWord">名前フィルタ</label>
@@ -169,7 +171,7 @@ const ResultNewView: React.FC<{
       </div>
     </div>
     <div className="row mt-3 justify-content-center">
-      <div className="col-12 col-md-8">
+      <div className="col-12 col-md-10">
         <table className="border table table-striped table-nowrap table-bordered table-sm">
           <thead>
             <tr>
@@ -242,6 +244,8 @@ const UsedItemRecord: React.FC<{ record: UsedItem }> = ({ record }) => {
     <tr>
       <td className="align-middle"><a href={record.item_url} target="_blank" rel="noopener noreferrer">{record.name}</a></td>
       <td className="align-middle">{record.price}</td>
+      <td className="align-middle">{record.shop_name}</td>
+      <td className="align-middle">{record.rank}</td>
       <td className="align-middle"><img src={record.image_url} width={THUMBNAIL_SIZE} height={THUMBNAIL_SIZE} alt={record.name} /></td>
       <td className="align-middle text-center"><button className="btn btn-primary"
         onClick={() => setShowStockFlg(!showStockFlg)}>状態</button></td>
@@ -303,6 +307,12 @@ const ResultUsedView: React.FC<{
       case '価格':
         temp = temp.sort((a, b) => a.price > b.price ? 1 : a.price < b.price ? -1 : 0);
         break;
+      case '販売店':
+        temp = temp.sort((a, b) => a.shop_name > b.shop_name ? 1 : a.shop_name < b.shop_name ? -1 : 0);
+        break;
+      case 'ランク':
+        temp = temp.sort((a, b) => a.rank > b.rank ? 1 : a.rank < b.rank ? -1 : 0);
+        break;
     }
     if (sortDescFlg) {
       temp = temp.reverse();
@@ -310,36 +320,16 @@ const ResultUsedView: React.FC<{
     return temp;
   };
 
-  const itemNameLabel = () => {
-    if (sortKey !== '商品名') {
-      return '商品名';
+  const itemLabel = (label: string) => {
+    if (sortKey !== label) {
+      return label;
     }
-    return '商品名' + (sortDescFlg ? '↓' : '↑');
+    return label + (sortDescFlg ? '↓' : '↑');
   }
 
-  const itemPriceLabel = () => {
-    if (sortKey !== '価格') {
-      return '価格';
-    }
-    return '価格' + (sortDescFlg ? '↓' : '↑');
-  }
-
-  const onClickItemNameLabel = () => {
-    if (sortKey !== '商品名') {
-      setSortKey('商品名');
-      setSortDescFlg(false);
-    } else {
-      if (!sortDescFlg) {
-        setSortDescFlg(true);
-      } else {
-        setSortKey('');
-      }
-    }
-  };
-
-  const onClickItemPriceLabel = () => {
-    if (sortKey !== '価格') {
-      setSortKey('価格');
+  const onClickItemLabel = (label: string) => {
+    if (sortKey !== label) {
+      setSortKey(label);
       setSortDescFlg(false);
     } else {
       if (!sortDescFlg) {
@@ -352,7 +342,7 @@ const ResultUsedView: React.FC<{
 
   return (<>
     <div className="row mt-3 justify-content-center">
-      <div className="col-12 col-md-8">
+      <div className="col-12 col-md-10">
         <form>
           <div className="form-group d-flex mt-0">
             <label className="text-nowrap mr-3 mt-2" htmlFor="filterWord">名前フィルタ</label>
@@ -364,12 +354,14 @@ const ResultUsedView: React.FC<{
       </div>
     </div>
     <div className="row justify-content-center">
-      <div className="col-12 col-md-8">
+      <div className="col-12 col-md-10">
         <table className="border table table-striped table-nowrap table-bordered table-sm">
           <thead>
             <tr>
-              <th className="text-nowrap" onClick={onClickItemNameLabel}>{itemNameLabel()}</th>
-              <th className="text-nowrap" onClick={onClickItemPriceLabel}>{itemPriceLabel()}</th>
+              <th className="text-nowrap" onClick={() => onClickItemLabel('商品名')}>{itemLabel('商品名')}</th>
+              <th className="text-nowrap" onClick={() => onClickItemLabel('価格')}>{itemLabel('価格')}</th>
+              <th className="text-nowrap" onClick={() => onClickItemLabel('販売店')}>{itemLabel('販売店')}</th>
+              <th className="text-nowrap" onClick={() => onClickItemLabel('ランク')}>{itemLabel('ランク')}</th>
               <th className="text-nowrap">画像</th>
               <th className="text-nowrap">詳細</th>
             </tr>
@@ -450,12 +442,12 @@ const App: React.FC = () => {
   return (
     <div className="container">
       <div className="row mt-3 justify-content-center">
-        <div className="col-12 col-md-8 text-center">
+        <div className="col-12 col-md-10 text-center">
           <h1>eイヤ検索アプリ</h1>
         </div>
       </div>
       <div className="row mt-3 justify-content-center">
-        <div className="col-12 col-md-8">
+        <div className="col-12 col-md-10">
           <form className="border p-3">
             <div className="form-group">
               <label htmlFor="searchWord">検索ワード</label>
@@ -476,7 +468,7 @@ const App: React.FC = () => {
         </div>
       </div>
       <div className="row mt-3 justify-content-center">
-        <div className="col-12 col-md-8">
+        <div className="col-12 col-md-10">
           <div className={`spinner-border text-primary ${loadIngFlg ? '' : 'd-none'}`} role="status">
             <span className="sr-only">Loading...</span>
           </div>
