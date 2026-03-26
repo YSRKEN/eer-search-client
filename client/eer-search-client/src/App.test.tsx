ea0414f5d2cd82b184d37bc311503404db4505b1
YSRKEN <ysr.ken@gmail.com>
2026-03-26 23:28:51 +0900

    feat(client): migrate from CRA to Vite

--- client/eer-search-client/src/App.test.tsx
+++ client/eer-search-client/src/App.test.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import App from 'App';
+import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('タイトルが表示される', () => {
+  const { getByRole } = render(<App />);
+  expect(getByRole('heading', { name: 'eイヤ検索アプリ' })).toBeInTheDocument();
 });
